5650f4ef7ef90553b8cea784e6144fddc77928a7
linnasai521-star <linnasai521@gmail.com>
2026-05-14 15:29:31 +0800

    refactor: 新建layers/FloatingContent组件壳

---
+++ src/components/layers/FloatingContent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './FloatingContent.css';
+
+const h = React.createElement;
+
+export default function FloatingContent({ stats = [], scene }) {
+  return h('div', { className: 'floatingArea' },
+    stats.length > 0
+      ? h('div', { className: 'statsRow' },
+          stats.map((s, i) =>
+            h('div', { key: i, className: 'statCard' },
+              h('div', { className: 'statValue' }, s.value),
+              h('div', { className: 'statLabel' }, s.label)
+            )
+          )
+        )
+      : null,
+    scene ? h('div', { className: 'sceneText' }, scene) : null
+  );
+}
